f124732ad0dcbc791a3c2458e0c58a585cfcbb14
Joe Dixon <joedixon21@gmail.com>
2024-11-05 09:21:23 +0000

    Added body propert to article parametric endpoint

--- models/articles.js
+++ models/articles.js
@@ -7,7 +7,7 @@ exports.fetchArticlesById = (article_id) => {
     return db
         .query(
             `
-            SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(comment_id) AS INT) AS comment_count
+            SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(comment_id) AS INT) AS comment_count
 		    FROM articles LEFT OUTER JOIN comments ON articles.article_id = comments.article_id
             WHERE articles.article_id = $1
             GROUP BY articles.article_id;
